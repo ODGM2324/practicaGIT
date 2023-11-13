661ccb4968b25124530ff570067c9cec7c59c5f7
ODGM2324 <osgimu2@gmail.com>
2023-11-13 18:43:01 +0100

    Formulario diseñado y código del programa.

--- Practica3.1.GitHub/Form1.Designer.cs
+++ Practica3.1.GitHub/Form1.Designer.cs
@@ -3,14 +3,14 @@
     partial class Form1
     {
         /// <summary>
-        /// Variable del diseñador necesaria.
+        ///  Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Limpiar los recursos que se estén usando.
+        ///  Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,21 +20,101 @@
             base.Dispose(disposing);
         }
 
-        #region Código generado por el Diseñador de Windows Forms
+        #region Windows Form Designer generated code
 
         /// <summary>
-        /// Método necesario para admitir el Diseñador. No se puede modificar
-        /// el contenido de este método con el editor de código.
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            txtTelegrama = new TextBox();
+            cbUrgente = new CheckBox();
+            label2 = new Label();
+            txtPrecio = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(25, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(39, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Texto";
+            // 
+            // txtTelegrama
+            // 
+            txtTelegrama.Location = new Point(25, 53);
+            txtTelegrama.Multiline = true;
+            txtTelegrama.Name = "txtTelegrama";
+            txtTelegrama.Size = new Size(374, 100);
+            txtTelegrama.TabIndex = 1;
+            // 
+            // cbUrgente
+            // 
+            cbUrgente.AutoSize = true;
+            cbUrgente.Location = new Point(25, 183);
+            cbUrgente.Name = "cbUrgente";
+            cbUrgente.Size = new Size(73, 19);
+            cbUrgente.TabIndex = 2;
+            cbUrgente.Text = "Urgente?";
+            cbUrgente.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(25, 260);
+            label2.Name = "label2";
+            label2.Size = new Size(41, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Coste:";
+            // 
+            // txtPrecio
+            // 
+            txtPrecio.Location = new Point(69, 257);
+            txtPrecio.Name = "txtPrecio";
+            txtPrecio.Size = new Size(132, 23);
+            txtPrecio.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(272, 231);
+            button1.Name = "button1";
+            button1.Size = new Size(127, 49);
+            button1.TabIndex = 5;
+            button1.Text = "Calcular";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(427, 309);
+            Controls.Add(button1);
+            Controls.Add(txtPrecio);
+            Controls.Add(label2);
+            Controls.Add(cbUrgente);
+            Controls.Add(txtTelegrama);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox txtTelegrama;
+        private CheckBox cbUrgente;
+        private Label label2;
+        private TextBox txtPrecio;
+        private Button button1;
     }
 }
-
--- Practica3.1.GitHub/Form1.cs
+++ Practica3.1.GitHub/Form1.cs
@@ -1,13 +1,3 @@
-ï»¿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
 namespace Practica3._1.GitHub
 {
     public partial class Form1 : Form
@@ -15,6 +5,37 @@ namespace Practica3._1.GitHub
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string textoTelegrama;
+            char tipoTelegrama = ' ';
+            int numPalabras = 0;
+            double coste;
+            //Leo el telegrama
+            textoTelegrama = txtTelegrama.Text;
+            // telegrama urgente?
+            if (cbUrgente.Checked)
+                tipoTelegrama = 'u';
+            //Obtengo el número de palabras que forma el telegrama
+            numPalabras = textoTelegrama.Length;
+            //Si el telegrama es ordinario
+            if (tipoTelegrama == 'o')
+                if (numPalabras <= 10)
+                    coste = 25;
+                else
+                    coste = 0.5 * numPalabras;
+            else
+            //Si el telegrama es urgente
+            if (tipoTelegrama == 'u')
+                if (numPalabras <= 10)
+                    coste = 5;
+                else
+                    coste = 5 + 0.75 * (numPalabras - 10);
+            else
+                coste = 0;
+            txtPrecio.Text = coste.ToString() + " euros";
         }
     }
 }
